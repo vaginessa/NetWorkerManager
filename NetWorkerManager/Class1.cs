c9667cf98d486aa75bd710f967d67221707c6cfa
495151742 <495151742@qq.com>
2018-12-25 13:42:04 +0800

    添加项目文件。

---
+++ NetWorkerManager/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetWorkerManager
+{
+    public class Class1
+    {
+    }
+}
